460d1d1c8f3475213748969b8a2b2fe39d664feb
Vitali Slepnyov <icofundss@gmail.com>
2024-05-07 23:58:59 +0300

    feat: add unit creation

--- core.js
+++ core.js
@@ -2,6 +2,26 @@ module.exports.loop = function () {
     // Получить список всех юнитов
     const creeps = Game.creeps;
 
+    // Проверить, есть ли юниты каждого типа
+    const roles = ['harvester', 'upgrader', 'builder', 'repairer'];
+    let hasAllRoles = true;
+    for (const role of roles) {
+        if (!creeps.some(creep => creep.memory.role === role)) {
+            hasAllRoles = false;
+            break;
+        }
+    }
+
+    
+    // Если нет юнитов каждого типа, создать их
+    if (!hasAllRoles) {
+        const spawn = Game.spawns['Spawn1'];
+        if (spawn.energy >= 300) {
+            const role = roles[Math.floor(Math.random() * roles.length)];
+            spawn.createCreep([WORK, CARRY, MOVE], `${role}-${Game.time}`, { role });
+        }
+    }
+
     // Пройтись по каждому юниту
     for (const name in creeps) {
         const creep = creeps[name];
